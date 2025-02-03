682c77bdaa8be4ea778369e83b14a3f188bd5ecc
Noel Pactanac <202280151@psu.palawan.edu.ph>
2025-02-03 22:22:52 +0800

    add frequency and sort function

--- process.php
+++ process.php
@@ -1,36 +1,16 @@
 <?php
 header('Content-Type: application/json');
 
-/**
- * Word Frequency Counter Process
- * 
- * Get the user input text
- * List the words to ignore
- * 
- * The Tokenizer: Function to extract words from the text and remove ignored words, 
- * and exclude the following:
- * Spaces
- * Punctuation
- * Line breaks
- * Other marks like question mark
- * Periods
- * 
- * Word Frequency Calculator function
- * It will count the frequency of each unique word
- * 
- * Limiter
- * It limits the result based on the user input
- * Also, it already gets the top N most frequent words before limiting 
- * 
- * Sorting by Order
- * It will consider the choice of the user to list the words based on frequency of its count
- */
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $user_text = strval($_POST['text']);
 
-    $ignore_word_list = ['the', 'and', 'in', 'is', 'it', 'to', 'of', 'a', 'for', 'on', 'with', 'as', 'that', 'by', 'at', 'be', 'this', 'from', 'or', 'an', 'if','i','ve','m','s','me','my','was','t',];
+    $ignore_word_list = ['the', 'and', 'in', 'is', 'it', 'to', 'of', 'a', 'for', 'on', 'with', 'as', 'that', 'by', 'at', 'be', 'this', 'from', 'or', 'an', 'if','i','ve','m','s','me','my','was','t','can','you'];
+
+    $sortOrder = $_POST['sort'];
+
+    $displayLimit = $_POST['limit'];
 
     function word_tokenizer ($text, $ignore_word_list) {
         // convert everything to lowercase
@@ -38,10 +18,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         //use regex to ignore special characters
         $text = preg_replace('/([^a-zA-z0-9]+)/', ' ', $text);
+
+        //explode function separates the words and converts it into an array using whitespace as separator
         $words = explode(' ', $text);
+
+        //array_diff compare contents and ignore words from the list of set ignored word array
         $words = array_diff($words, $ignore_word_list);
+        
+        //return of array of words, still have dupes
         return $words;
     }
+
+    $tokenized_json = word_tokenizer($user_text, $ignore_word_list);
     
-    echo json_encode(word_tokenizer($user_text, $ignore_word_list));
+
+
+    //WORD FREQUENCY CALC
+    function frequency_calc($word_array){
+        $word_freq = array_count_values($word_array);
+        return $word_freq;
+    }
+
+    //variable for array where each occurences are tallied
+    $word_freq_array = frequency_calc($tokenized_json);
+
+    //ascension sorting
+    function sort_array($tallied_array,$sortOrder){
+        if($sortOrder == 'asc'){
+            asort($tallied_array);
+        }
+        else if ($sortOrder == 'desc')
+        {
+            arsort($tallied_array);
+        }
+        return $tallied_array;
+    }
+    //array sorted by its frequency
+    $sorted_array = sort_array($word_freq_array,$sortOrder);
+
+    //display only based on limit
+
+    function display_array_with_limit(){
+
+    }
+
+
+    echo json_encode($sorted_array);
+
 }
